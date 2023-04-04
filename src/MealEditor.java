abbacb461d47b8e9f53802f12ae3cd6b0ea532f7
Julian Barrett <barre3jr@dukes.jmu.edu>
2023-04-04 16:30:17 -0400

    Merge branch 'main' of https://github.com/bernstdh/S23Team3C into main

--- src/MealEditor.java
+++ src/MealEditor.java
@@ -1,6 +1,13 @@
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.awt.BorderLayout;
 
 import javax.swing.BorderFactory;
@@ -12,19 +19,24 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import javax.swing.plaf.synth.ColorType;
 import java.awt.Color;
 import java.awt.Dimension;
 
 
 
-public class MealEditor extends JFrame {
+public class MealEditor extends JFrame implements ActionListener {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JLabel nameLabel;
 	private JTextField nameBox;
 	private JTextArea recipeTextBox;
 	private JButton addRecipeButton;
 	private JButton deleteRecipeButton;
+	
+	// private 
 
 
 	private JButton newButton;
@@ -46,19 +58,22 @@ public class MealEditor extends JFrame {
 		
 		Dimension buttonSize = new Dimension(30, 30);
 		
-		newButton = new JButton(new ImageIcon("baseline_add_box_black_24dp.png"));
+		newButton = new JButton(new ImageIcon("newButton.png"));
 		newButton.setPreferredSize(buttonSize);
-		openButton = new JButton(new ImageIcon("baseline_file_open_black_24dp.png"));
+		openButton = new JButton(new ImageIcon("openButton.png"));
 		openButton.setPreferredSize(buttonSize);
-		saveButton = new JButton(new ImageIcon("baseline_save_black_24dp.png"));
+		saveButton = new JButton(new ImageIcon("saveButton.png"));
 		saveButton.setPreferredSize(buttonSize);
-		saveAsButton = new JButton(new ImageIcon("baseline_save_as_black_24dp.png"));
+		saveAsButton = new JButton(new ImageIcon("saveAsButton.png"));
 		saveAsButton.setPreferredSize(buttonSize);
-		closeButton = new JButton(new ImageIcon("baseline_close_black_24dp.png"));
+		closeButton = new JButton(new ImageIcon("closeButton.png"));
 		closeButton.setPreferredSize(buttonSize);
 
 		
 		this.setLayout(new BorderLayout());
+		// changed layout to grid
+		//this.setLayout(new GridLayout(2, 0));
+		
 		// buttons on the top
 	    JPanel p = new JPanel();
 	    p.setLayout(new FlowLayout(FlowLayout.LEFT));
@@ -79,40 +94,76 @@ public class MealEditor extends JFrame {
 		JPanel borderPnl = new JPanel(new BorderLayout());
 		borderPnl.add(p, BorderLayout.NORTH);
 		borderPnl.add(nameFrame, BorderLayout.CENTER);
-
-		this.add(borderPnl);
+		this.add(borderPnl, BorderLayout.NORTH);
 		
 		// JPanel with the add recipe and delete button	
 		JPanel recipeBigPanel = new JPanel(new BorderLayout());
 		
 		JPanel addRecipeFlow = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		addRecipeButton = new JButton("Add Recipe");
+		addRecipeButton.addActionListener(this);
 		addRecipeFlow.add(addRecipeButton);
-		recipeBigPanel.add(addRecipeFlow, BorderLayout.NORTH);
-		
-		recipeBigPanel.add(new JScrollPane(recipeTextBox), BorderLayout.CENTER);
+				
+		recipeTextBox = new JTextArea ( 15, 25 );
+	    recipeTextBox.setEditable ( false ); // set textArea non-editable
+	    JScrollPane scroll = new JScrollPane (recipeTextBox);
+	    scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        addRecipeFlow.add(scroll, BorderLayout.CENTER);
+	    
+        JPanel deletePanel = new JPanel(new BorderLayout());
 		deleteRecipeButton = new JButton("Delete");
-		recipeBigPanel.add(deleteRecipeButton, BorderLayout.CENTER);
-		
+		deleteRecipeButton.addActionListener(this);
+		deletePanel.add(deleteRecipeButton, BorderLayout.SOUTH);
+		addRecipeFlow.add(deletePanel);
+
+		recipeBigPanel.add(addRecipeFlow, BorderLayout.CENTER);
 		recipeBigPanel.setBorder(BorderFactory.createLineBorder(Color.black));
 		
-		this.add(recipeBigPanel, BorderLayout.SOUTH);
-		
+		this.add(recipeBigPanel); 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(500, 400);
 		setVisible(true);
-		
-		// ACTION LISTENERS!!!
-//		
-//		newButton.addActionListener(new ActionListener() 
-//		{
-//			public void actionPerformed(final ActionEvent e)
-//			{
-//				generateShoppingList(strings, num);
-//			}
-//		});
 
 	}
+	
+	  @Override
+	  public void actionPerformed(final ActionEvent e)
+	  {
+	    if (e.getSource() == addRecipeButton)
+	    {
+	      // new ;
+	    } else if (e.getSource() == deleteRecipeButton) {
+	    	
+	    }
+	  }
+	
+	   public void write(String filename) throws IOException
+	   {
+	     ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filename + ".mel"));
+	     out.writeObject(this);
+	     out.flush();
+	     out.close();
+	   }
+	   
+	   
+	   // return value is a File Object?
+	   // maybe return value should be a meal
+	    public static File open(String filename) throws IOException
+	   {
+	     ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename + ".txt"));
+	     File file;
+	     try
+	     {
+	       file = (File)in.readObject();
+	     }
+	     catch (ClassNotFoundException cnfe)
+	     {
+	       file = new File("Unknown");
+	     }
+	     in.close();
+	     
+	     return file;
+	   }
 	
 	public static void main(String[] args) {
 		new MealEditor();
--- src/Meals.java
+++ src/Meals.java
@@ -1,15 +1,48 @@
 import java.util.List;
 
+/**
+ * Meals class creates a list of recipes.
+ * @author Julian Barrett
+ *
+ */
 public class Meals {
 
 	private List<Recipes> recipes;
+	private String name;
 
-	public Meals(List<Recipes> recipes) 
+	/** Meals constructor
+	 * @param recipes for list of recipes.
+	 */
+	public Meals(String name, List<Recipes> recipes)
 	{
+		this.name = name;
 		this.recipes = recipes;
 	}
+	
+	/**
+	 * returns name;
+	 * @return name.
+	 */
+	public String getName()
+	{
+		return this.name;
+	}
+	
+	/**
+	 * returns recipes.
+	 * @return recipes;
+	 */
+	public List<Recipes> getRecipes() 
+	{
+		return this.recipes;
+	}
 
-	public void addRecipe(Recipes recipe) {
+	/** 
+	 * Add a recipe to the list.
+	 * @param recipe to be added.
+	 */
+	public void addRecipe(Recipes recipe) 
+	{
 		this.recipes.add(recipe);
 	}
 }
